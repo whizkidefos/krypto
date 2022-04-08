4f3efa4fd1cd887f651d4f1de1884bae68f18d8e
Efosa Igbinehi <whizkidefos@users.noreply.github.com>
2022-04-08 07:37:35 +0100

    Welcome component UI

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
-
-const Loader = () => {
-    return (
-        <div>Loader</div>
-    )
-}
+const Loader = () => (
+    <div className="flex items-center justify-center py-3">
+        <div className="w-24 h-24 border-b-2 border-red-700 rounded-full animate-spin" />
+    </div>
+);
 
 export default Loader;
