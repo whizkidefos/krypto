4b24225a34bc8acb11abab5678a7e5a1dae7f825
Efosa Igbinehi <whizkidefos@users.noreply.github.com>
2022-04-10 14:57:03 +0100

    reinstalled dev dependecies

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,7 +1,7 @@
 require("@nomiclabs/hardhat-waffle");
 
 module.exports = {
-  solidity: "0.8.4",
+  solidity: "0.8.0",
   networks: {
     ropsten: {
       url: 'https://eth-ropsten.alchemyapi.io/v2/DwsWwafFUqVxu0D__Q0OQBdH-eEzEbyK',
